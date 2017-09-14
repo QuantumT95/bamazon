8ae41087ec74836c6ce40374b430578342b671a5
QuantumT95 <tran.charlie@ymail.com>
2017-09-14 10:40:34 -0500

    made the first customer view, gotta have a way to update database and then do math to show how much money the customer owes

---
+++ bamazonCustomer.js
@@ -0,0 +1,88 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect();
+
+// Display all of the items available for sale
+connection.query("SELECT item_id, product_name, price FROM products;", function(err, res) {
+    if (err) throw err;
+    console.log(res);
+    start();
+});
+
+function start() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "Which product would you like to buy?"
+        },
+        {
+          name: "quantity",
+          type: "input",
+          message: "How much of the product would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if bid was high enough
+        if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+          // bid was high enough, so update db, let the user know, and start over
+          subtract = chosenItem.stock_quantity - parseInt(answer.quantity);
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stock_quantity: subtract
+              },
+              {
+                item_id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("order has been placed successfully!");
+              // how much of the product left we have in stack after purchase
+			  console.log(subtract);
+            }
+          );
+        }
+        else {
+         // Not enough in stock
+        console.log("We don't have enough in stock, sorry :( ");
+        }
+      });
+  });
+}
